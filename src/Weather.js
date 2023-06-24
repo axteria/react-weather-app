bc6d25d14e413438a9a6c949df3bd6ea031b8d6f
axteria <fayee.wu@hotmail.com>
2023-06-24 16:10:45 +1000

    try debug fail to host

--- src/Weather.js
+++ src/Weather.js
@@ -13,22 +13,6 @@ export default function Weather() {
         <li>Wednesday 07:00</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          6°C
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity 72%</li>
-            <li>Wind: 13 km/h</li>
-          </ul>
-        </div>
-      </div>
     </div>
   );
 }
