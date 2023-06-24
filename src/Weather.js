060902f494248daf7320c0cf41826495efee92c9
axteria <fayee.wu@hotmail.com>
2023-06-24 15:55:51 +1000

    added footer

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,13 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
     <div>
       <h1>testing</h1>
+      <footer>
+        Open-sourced by Faye.
+      </footer>
     </div>
   );
 }
