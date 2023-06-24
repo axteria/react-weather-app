eb7c0390f78d69819ce3668bb8d7a9687e8561b3
axteria <fayee.wu@hotmail.com>
2023-06-24 16:07:07 +1000

    added basic html

--- src/Weather.js
+++ src/Weather.js
@@ -3,8 +3,32 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div>
-      <h1>testing</h1>
+    <div className="weather">
+      <form>
+        <input />
+      </form>
+
+      <h1>Melbourne</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly Cloudy"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity 72%</li>
+            <li>Wind: 13 km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
